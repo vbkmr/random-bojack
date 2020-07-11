4ad0a454d5e6ec0215a0cbae917bdd69cf9de840
ShilpiMaurya <47273243+ShilpiMaurya@users.noreply.github.com>
2020-07-11 20:41:36 +0530

    feature/RB-1 (#3)
    * random-bojack app first look
    
    * refactored the code
    
    * fixed error
    
    * some style changes
    
    * fixed some error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,68 @@
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import axios from "axios";
 
-const Title = styled.h1`
-  color: red;
-  font-size: 50px;
+const DisplayImage = styled.div<{ image: string }>`
+  background-image: url("${props => props.image}");
+  width: 100%;
+  height: 800px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
-export default function Home() {
-  return <Title>Random bojack project</Title>;
+interface ImageUrl {
+  urls: {
+    full: string;
+  };
 }
+
+interface ImageApiResponse {
+  data: {
+    results: [ImageUrl];
+  };
+}
+
+const Home: React.FC = () => {
+  const [horsePic, setHorsePic] = useState<string>("");
+  const [imageUrls, setImageUrls] = useState<[ImageUrl] | null>(null);
+  
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.unsplash.com/search/photos?&query=horse&client_id=qwsMPXru1mQVCIU13RhAMYvJGzWwIpjfVsICqncS-m4"
+      )
+      .then((response: ImageApiResponse) => {
+        setImageUrls(response.data.results);
+      })
+      .catch((error: ErrorConstructor) => {
+        console.log(error);
+      });
+  }, []);
+
+  let index = 0;
+  const setIntervalFunction = () => {
+    if (imageUrls !== null) {
+      if (index <= imageUrls.length - 1) {
+         setHorsePic(imageUrls[index].urls.full);
+         index = index + 1;
+      } else {
+        setHorsePic(imageUrls[0].urls.full);
+        index = 1;
+      }
+    }
+  };
+  useEffect(() => {
+    const interval = setInterval(setIntervalFunction, 10000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [imageUrls]);
+
+  return (
+    <>
+      <DisplayImage image={horsePic} />
+    </>
+  );
+};
+export default Home;
