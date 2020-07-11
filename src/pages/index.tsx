1857c88ad8747038509d04538ebbde801f8f0fce
ShilpiMaurya <meshilpi94@gmail.com>
2020-07-11 14:08:08 +0530

    refactored the code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
-const DisplayImage = styled.div<{ image: string }>`
+
+const DisplayImage = styled.div<{ image: string[] }>`
   background-image: url("${props => props.image}");
   width: 100%;
   height: 800px;
@@ -9,31 +10,53 @@ const DisplayImage = styled.div<{ image: string }>`
   background-repeat: no-repeat;
   background-size: cover;
 `;
+
+interface ImageUrl {
+  urls: {
+    regular: [string];
+  };
+}
+
+interface ImageApiResponse {
+  data: {
+    results: [ImageUrl];
+  };
+}
+
 const Home: React.FC = () => {
-  const [horsePic, setHorsePic] = useState<string>("");
-  const [index, setIndex] = useState<number>(0);
+  const [horsePic, setHorsePic] = useState<string[]>([]);
+  const [imageUrls, setImageUrls] = useState<[ImageUrl] | null>(null);
   useEffect(() => {
-    const interval = setInterval(() => {
-      axios
-        .get(
-          "https://api.unsplash.com/search/photos?&query=horse&client_id=qwsMPXru1mQVCIU13RhAMYvJGzWwIpjfVsICqncS-m4"
-        )
-        .then(response => {
-          if (index <= 9) {
-            setIndex(index + 1);
-            setHorsePic(response.data.results[index].urls.regular);
-          } else {
-            setIndex(0);
-          }
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }, 10000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [index]);
+    axios
+      .get(
+        "https://api.unsplash.com/search/photos?&query=horse&client_id=qwsMPXru1mQVCIU13RhAMYvJGzWwIpjfVsICqncS-m4"
+      )
+      .then((response: ImageApiResponse) => {
+        setImageUrls(response.data.results);
+      })
+      .catch((error: ErrorConstructor) => {
+        console.log(error);
+      });
+  }, []);
+
+  let index = 0;
+  useEffect(() => {
+    if (imageUrls !== null) {
+      const interval = setInterval(() => {
+        if (index <= imageUrls.length - 1) {
+          index = index + 1;
+          setHorsePic(imageUrls[index].urls.regular);
+          console.log( "shilpi");
+        } else {
+          index = 0;
+        }
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [imageUrls]);
+
   return (
     <>
       <DisplayImage image={horsePic} />
